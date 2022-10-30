75af739ce67f5df7450da458a571b1e78166d412
ryanoliveir <ryandehallo229@gmail.com>
2022-10-30 18:47:00 -0300

    Types fix

--- lib/db.ts
+++ lib/db.ts
@@ -12,7 +12,7 @@ export async function getAllTodos(){
 }
 
 
-export async function createTodo(description: String){
+export async function createTodo(description: string){
     await prisma.todo.create({
         data:{
             description,
